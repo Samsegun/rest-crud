2c73f5c0a9dcdc994c1538814de69343891f5285
Samsegun <oyebadesamsegun@gmail.com>
2024-06-15 21:54:56 +0100

    spring data jpa layer

---
+++ src/main/java/com/samsegun/restdemo/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.samsegun.restdemo.dao;
+
+import com.samsegun.restdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
--- src/main/java/com/samsegun/restdemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/samsegun/restdemo/service/EmployeeServiceImpl.java
@@ -1,40 +1,53 @@
 package com.samsegun.restdemo.service;
 
-import com.samsegun.restdemo.dao.EmployeeDAO;
+import com.samsegun.restdemo.dao.EmployeeRepository;
+import com.samsegun.restdemo.dao.EntityException;
 import com.samsegun.restdemo.entity.Employee;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findEmployee(Integer id) {
-        return employeeDAO.findEmployee(id);
+        return checkForEmployee(id);
     }
 
-    @Transactional
     @Override
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
-    @Transactional
     @Override
     public void deleteEmployee(Integer id) {
-        employeeDAO.deleteEmployee(id);
+        Employee employee = checkForEmployee(id);
+        employeeRepository.deleteById(employee.getId());
+    }
+
+    private Employee checkForEmployee(Integer id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee theEmployee = null;
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            throw new EntityException("Employee not found with id: " + id, "NOT FOUND");
+        }
+
+        return theEmployee;
     }
 }
